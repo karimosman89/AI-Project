643b1971fea047776253a51414088d8b8953b159
Karim Osman <60973591+karimosman89@users.noreply.github.com>
2024-09-21 10:52:56 +0200

    Update data_ingestion.py

--- dags/data_ingestion.py
+++ dags/data_ingestion.py
@@ -1,22 +1,6 @@
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
-import boto3
-import pandas as pd
-
-# AWS credentials
-s3 = boto3.client('s3')
-
-def download_data():
-    # Download CIFAR-10 dataset or use Kaggle API
-    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
-    pd.DataFrame(X_train).to_csv('/tmp/X_train.csv')
-    pd.DataFrame(y_train).to_csv('/tmp/y_train.csv')
-
-def upload_to_s3():
-    # Upload to AWS S3
-    s3.upload_file('/tmp/X_train.csv', 'your-s3-bucket', 'X_train.csv')
-    s3.upload_file('/tmp/y_train.csv', 'your-s3-bucket', 'y_train.csv')
 
 default_args = {
     'owner': 'airflow',
@@ -24,15 +8,25 @@ default_args = {
     'retries': 1
 }
 
-with DAG(dag_id='data_ingestion', default_args=default_args, schedule_interval='@daily') as dag:
-    task1 = PythonOperator(
-        task_id='download_data',
-        python_callable=download_data
-    )
-    task2 = PythonOperator(
-        task_id='upload_to_s3',
-        python_callable=upload_to_s3
-    )
+dag = DAG(
+    'data_ingestion',
+    default_args=default_args,
+    schedule_interval='@daily'
+)
 
-    task1 >> task2
+# Download CIFAR-10 data
+download_data = BashOperator(
+    task_id='download_cifar10',
+    bash_command="curl -o /data/raw/train.7z <CIFAR-10-dataset-url>",
+    dag=dag
+)
+
+# Extract train and test images
+extract_data = BashOperator(
+    task_id='extract_data',
+    bash_command='7z x /data/raw/train.7z -o/data/raw/',
+    dag=dag
+)
+
+download_data >> extract_data
 
