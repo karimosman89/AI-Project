b1f6852be14a0bb5c5a54093f245921ae1c3ca43
Karim Osman <60973591+karimosman89@users.noreply.github.com>
2024-09-21 11:04:49 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,19 @@
 import os
 
 class Config:
-    MODEL_PATH = os.getenv('MODEL_PATH', 'src/model/model.h5')
-    DATA_PATH = os.getenv('DATA_PATH', 'data/processed/')
-    EPOCHS = int(os.getenv('EPOCHS', 10))
-    BATCH_SIZE = int(os.getenv('BATCH_SIZE', 32))
-    FLASK_HOST = os.getenv('FLASK_HOST', '0.0.0.0')
-    FLASK_PORT = int(os.getenv('FLASK_PORT', 5000))
+    # Paths
+    MODEL_PATH = os.getenv("MODEL_PATH", "/app/src/model/model.h5")
+    DATA_PATH = os.getenv("DATA_PATH", "/app/data/processed/")
+
+    # Training Parameters
+    EPOCHS = int(os.getenv("EPOCHS", 10))
+    BATCH_SIZE = int(os.getenv("BATCH_SIZE", 32))
+
+    # Flask Server Config
+    FLASK_HOST = os.getenv("FLASK_HOST", "0.0.0.0")
+    FLASK_PORT = int(os.getenv("FLASK_PORT", 5000))
+
+    # MLFlow tracking URI
+    MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5001")
+
+config = Config()
